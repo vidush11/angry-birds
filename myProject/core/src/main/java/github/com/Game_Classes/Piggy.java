3a9d3ec7d8a6f5ddf4dbd5e43293b0873397cf91
Vidush Jindal <147984123+vidush11@users.noreply.github.com>
2024-12-01 00:07:09 +0530

    Update Piggy.java

--- myProject/core/src/main/java/github/com/Game_Classes/Piggy.java
+++ myProject/core/src/main/java/github/com/Game_Classes/Piggy.java
@@ -1,6 +1,8 @@
 package github.com.Game_Classes;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Filter;
 import com.badlogic.gdx.physics.box2d.World;
 import github.com.Main;
 
@@ -11,30 +13,29 @@ import java.util.concurrent.TimeUnit;
 
 public class Piggy extends PhysicsActor{
     private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-    public Piggy(World world, float x, float y, float width, float height) {
-        super(world, x, y, Main.assetManager.get("img/basicPig.png"), width, height, false);
-        getBody().setUserData(this);
+    public Piggy(World world, float x, float y, float width, float height, String s) {
+        super(world, x, y, new Texture(s), width, height, false);
+//        super.getUser
+        super.setHitPoints(5);
+        Filter filter=super.getBody().getFixtureList().get(0).getFilterData();
+        filter.categoryBits=Main.BIT_PIG;
+        super.getBody().getFixtureList().get(0).setFilterData(filter);
+        Object curr_user_data= super.getBody().getUserData();
+        super.getBody().setUserData(new userData((Sprite)curr_user_data, "piggy"));
     }
 
-    public void OnHit(ArrayList<Piggy> pigList, PhysicsActor obj){
-        setHitPoints(getHitPoints() - obj.getHitPoints());
-        System.out.println(getHitPoints());
-        if (getHitPoints() <= 0){
-            scheduler.schedule(() -> {
-                synchronized (this) {
-                    kill(pigList);
-                }
-            }, 1, TimeUnit.SECONDS);
-        };
+    public void OnHit(ArrayList<Piggy> pigList, Bird bird)
+    {
+//        setHitPoints(getHitPoints() - bird.getHitPoints());
+//        if (getHitPoints() <= 0){
+//            scheduler.schedule(() -> {
+//                synchronized (this) {
+//                    pigList.remove(this);
+//                    this.dispose();
+//                    this.remove();
+//                }
+//            }, 1, TimeUnit.SECONDS);
+//        };
     }
 
-    public void kill(ArrayList<Piggy> pigList){
-        pigList.remove(this);
-//        this.dispose();
-        this.remove();
-        if(getBody() != null && getBody().getWorld().getBodyCount()>0){
-            getBody().getWorld().destroyBody(getBody());
-        }
-    }
 }
-
