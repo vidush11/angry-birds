d15091bf5cd3fc529f24ce9d88db0c999aaffd31
Vidush Jindal <147984123+vidush11@users.noreply.github.com>
2024-12-01 00:06:22 +0530

    Update BuildingBlock.java

--- myProject/core/src/main/java/github/com/Game_Classes/BuildingBlock.java
+++ myProject/core/src/main/java/github/com/Game_Classes/BuildingBlock.java
@@ -1,36 +1,72 @@
 package github.com.Game_Classes;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.*;
 
-public abstract class Box2DActor extends Actor {
-    private final Body body;  // Box2D body for physics simulation
+import github.com.Main;
 
-    public Box2DActor(Body body) {
-        this.body = body;  // Set the body for the actor
-        setPosition(body.getPosition().x, body.getPosition().y);  // Set initial position
-        setRotation((float) Math.toDegrees(body.getAngle()));  // Set initial rotation
+public class BuildingBlock {
+    BodyDef bodyDef;
+    PolygonShape shape;
+    FixtureDef fixtureDef;
+    Body block;
+    Sprite boxSprite;
+    public static enum Type{
+        wood,
+        metal,
+        glass
+    }
+    private Type myType;
+    public BuildingBlock(World world, float x, float y, float width, float height, Type type, String s) {
+        bodyDef = new BodyDef();
+        shape=new PolygonShape();
+        fixtureDef= new FixtureDef();
+        boxSprite= new Sprite(new Texture(s));
+
+        //slingshot
+        bodyDef.type= BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(x,y);
+
+        shape.setAsBox(width/2.0f,height/2.0f);
+
+        fixtureDef.shape = shape;
+        fixtureDef.friction = .5f;
+        fixtureDef.restitution = 0.5f;
+        fixtureDef.density = 1f;
+        block= world.createBody(bodyDef);
+        block.createFixture(fixtureDef);
+
+        boxSprite.setSize(width,height);
+        boxSprite.setOrigin(boxSprite.getWidth()/2, boxSprite.getHeight()/2);
+        block.setUserData(new userData(boxSprite,"block"));
+//        super(world, x, y, new Texture(s), width, height, true);
+
+        this.myType = type;
+//        switch (this.myType){
+//            case wood:
+//                super.setHitPoints(5);
+//                break;
+//            case metal:
+//                super.setHitPoints(10);
+//                break;
+//            case glass:
+//                super.setHitPoints(2);
+//                break;
+//        }
+
+        shape.dispose();
     }
 
-    @Override
-    public void act(float delta) {
-        super.act(delta);
+    public void OnHit(PhysicsActor actor){
+//        this.setHitPoints(this.getHitPoints() - actor.getHitPoints());
+//        if (this.getHitPoints() <= 0){
+//            this.dispose();
+//            this.remove();
+//        }
+    };
 
-        // Update the position and rotation based on the Box2D body
-        setPosition(body.getPosition().x, body.getPosition().y);
-        setRotation((float) Math.toDegrees(body.getAngle()));
+    public void dispose(){
+
     }
-
-    @Override
-    public void draw(Batch batch, float parentAlpha) {
-        super.draw(batch, parentAlpha);
-    }
-
-    public Body getBody() {
-        return body;
-    }
-
-    public abstract void dispose();
 }
