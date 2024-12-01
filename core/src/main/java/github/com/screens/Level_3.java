801f88dc838e57fc44ccd2cbfe4aaec1e0f4ab7f
Vidush Jindal <vidush23592@iiitd.ac.in>
2024-12-02 00:00:54 +0530

    yoyo no changes

--- core/src/main/java/github/com/screens/Level_3.java
+++ core/src/main/java/github/com/screens/Level_3.java
@@ -370,8 +370,7 @@ public class Level_3 implements Screen {
                     score+=5000;
                 }
                 if (currBird!=null) score+=5000;
-
-//                System.out.println("SCORE: "+score);
+                
                 ((Game) Gdx.app.getApplicationListener()).setScreen(new WinLoose(game, score, 115000,true));
                 dispose();
             }
